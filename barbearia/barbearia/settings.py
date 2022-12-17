f5fb6df4f879bd8bd0da86536dd548cec0a67126
phaa <pedrohamorim2011@hotmail.com>
2022-12-17 16:29:20 -0300

    preparando para merge

--- barbearia/barbearia/settings.py
+++ barbearia/barbearia/settings.py
@@ -121,6 +121,12 @@ STATICFILES_DIRS = [
     'static/'
 ]
 
+AUTH_USER_MODEL = 'core.Usuario'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'perfil'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- barbearia/core/models.py
+++ barbearia/core/models.py
@@ -1,3 +1,62 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+ESTADOS = (
+    ('AC', 'Acre'),
+    ('AL', 'Alagoas'),
+    ('AP', 'Amapá'),
+    ('AM', 'Amazonas'),
+    ('BA', 'Bahia'),
+    ('CE', 'Ceará'),
+    ('DF', 'Distrito Federal'),
+    ('ES', 'Espírito Santo'),
+    ('GO', 'Goiás'),
+    ('MA', 'Maranhão'),
+    ('MT', 'Mato Grosso'),
+    ('MS', 'Mato Grosso do Sul'),
+    ('MG', 'Minas Gerais'),
+    ('PA', 'Pará'),
+    ('PB', 'Paraíba'),
+    ('PR', 'Paraná'),
+    ('PE', 'Pernambuco'),
+    ('PI', 'Piauí'),
+    ('RJ', 'Rio de Janeiro'),
+    ('RN', 'Rio Grande do Norte'),
+    ('RS', 'Rio Grande do Sul'),
+    ('RO', 'Rondônia'),
+    ('RR', 'Roraima'),
+    ('SC', 'Santa Catarina'),
+    ('SP', 'São Paulo'),
+    ('SE', 'Sergipe'),
+    ('TO', 'Tocantins'),
+)
+
+# permissoes = slide 74+
+class Usuario(AbstractUser):
+    nome = models.CharField('Nome Completo', max_length=100)
+    data_nascimento = models.DateField('Data de nascimento')
+    cpf = models.CharField('CPF', max_length=11, unique=True)
+    
+
+    # foreign para dizer se é barbeiro
+
+    USERNAME_FIELD = 'cpf'
+
+class Barbearia(models.Model):
+    id = models.UUIDField("Id", primary_key=True, default=uuid.uuid4, editable=False)
+    data_cadastro = models.DateField('Data de nascimento', auto_now_add=True)
+    cep = models.IntegerField('Cep')
+    logradouro = models.CharField('Logradouro', max_length=50)
+    numero = models.IntegerField('Número')
+    complemento = models.CharField('Complemento', max_length=100)
+    referencia = models.CharField('Referência', max_length=100)
+    bairro = models.CharField('Bairro', max_length=100)
+    cidade = models.CharField('Cidade', max_length=50)
+    estado = models.CharField("Estado", max_length=2, choices=ESTADOS)
+
+class Agendamento(models.Model):
+    id = models.UUIDField("Id", primary_key=True, default=uuid.uuid4, editable=False)
+    data_cadastro = models.DateField('Cadastrado em', auto_now_add=True) 
+    data_reserva = models.DateField('Data reservada') 
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
