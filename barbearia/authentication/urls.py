78b7e6b659cbf055d7c605f28e1d2dd2e2425092
phaa <pedrohamorim2011@hotmail.com>
2023-04-12 19:11:55 -0300

    Ajuste na pasta do projeto

--- barbearia/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-# Autenticação
-from .views import login, cadastro
-
-urlpatterns = [
-    path('login/', login, name='login'),
-    path('registrar/', cadastro, name='registro'),
-] 
